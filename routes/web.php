f163abc9c4df21f962d62b7261adefdef5d3902a
Sant <ignatius.varrel@gmail.com>
2024-12-16 22:56:25 +0700

    Menambahkan fitur Multi Auth pada branch feature/multi-auth-guard
    Catatan: Masih belum ada CRUD, search, dan Paging.

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,13 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware('auth:admin')->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return Inertia::render('Admin/Dashboard');
+    })->name('admin.dashboard');
+    
+    Route::get('/admin/profile', [ProfileController::class, 'edit'])->name('admin.profile.edit');
+});
+
 require __DIR__.'/auth.php';
+require __DIR__.'/AdminAuth.php';
