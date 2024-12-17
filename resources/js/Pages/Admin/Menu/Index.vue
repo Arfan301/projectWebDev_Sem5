c62b2247bf44f0b269f460ca480e8399ebe819cc
Sant <ignatius.varrel@gmail.com>
2024-12-17 21:25:27 +0700

    Add new feature Admin Search Paging from branch feature/multi-auth-guard-CRUD

--- resources/js/Pages/Admin/Menu/Index.vue
+++ resources/js/Pages/Admin/Menu/Index.vue
@@ -3,9 +3,39 @@
 
     <AdminAuthenticatedLayout>
         <template #header>
-            <div class="flex justify-between">
+            <div class="flex flex-col md:flex-row justify-between items-center">
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">Menu List</h2>
-                <Link :href="route('admin.menu.create')" class="bg-blue-500 text-white p-2 rounded">Add Menu</Link>
+                <!-- Search Form -->
+                <form class="flex items-center bg-gray-200 rounded-lg p-1 w-full md:w-auto mt-4 md:mt-0">
+                    <!-- Search Icon -->
+                    <span class="pl-3 text-gray-400 hidden md:block">
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                        </svg>
+                    </span>
+
+                    <!-- Input Field -->
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        id="search"
+                        name="search"
+                        class="flex-grow bg-transparent text-dark placeholder-gray-500 border-none px-3 w-full md:w-auto"
+                        :value="$page.props.search"
+                    />
+
+                    <!-- Search Button -->
+                    <button
+                        type="submit"
+                        class="bg-blue-500 text-white px-4 py-2 rounded-lg ml-2 focus:ring-2 focus:ring-blue-300"
+                    >
+                        <span class="hidden md:inline">Search</span>
+                        <svg class="w-6 h-6 md:hidden text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                        </svg>
+                    </button>
+                </form>
+                <Link :href="route('admin.menu.create')" class="bg-blue-500 text-white p-2 rounded mt-4 md:mt-0">Add Menu</Link>
             </div>
         </template>
 
@@ -14,51 +44,71 @@
         </div>
 
         <div class="mt-4 mx-4">
-            <table class="w-4/5 mx-auto bg-white border border-gray-200 shadow">
-                <thead>
-                    <tr>
-                        <th class="py-2 px-4 text-left border">Id</th>
-                        <th class="py-2 px-4 text-left border">Name</th>
-                        <th class="py-2 px-4 text-left border">Price</th>
-                        <th class="py-2 px-4 text-left border">View</th>
-                        <th class="py-2 px-4 text-left border">Edit</th>
-                        <th class="py-2 px-4 text-left border">Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr
-                        v-for="(item, index) in menus"
-                        :key="index"
-                    >
-                        <td class="py-2 px-4 border">{{ item.id }}</td>
-                        <td class="py-2 px-4 border">{{ item.name }}</td>
-                        <td class="py-2 px-4 border">{{ item.price }}</td>
-                        <td class="py-2 px-4 border">
-                            <Link 
-                                :href="route('admin.menu.show', item.id)" 
-                                class="px-2 oy-1 text-sm bg-blue-300 text-white me-2 rounded inline-block">
-                                Show
-                            </Link>
-                        </td>
-                        <td class="py-2 px-4 border">
-                            <Link 
-                                :href="route('admin.menu.edit', item.id)" 
-                                class="px-2 oy-1 text-sm bg-green-500 text-white me-2 rounded inline-block">
-                                Edit
-                            </Link>
-                        </td>
-                        <td class="py-2 px-4 border">
-                            <button
-                                type="submit"
-                                class="px-2 oy-1 text-sm bg-red-500 text-white me-2 rounded inline-block"
-                                @click="deleteMenu(item.id)"    
-                            >
-                                Delete
-                            </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="overflow-x-auto">
+                <table class="w-full bg-white border border-gray-200 shadow">
+                    <thead>
+                        <tr>
+                            <th class="py-2 px-4 text-left border">Id</th>
+                            <th class="py-2 px-4 text-left border">Name</th>
+                            <th class="py-2 px-4 text-left border">Price</th>
+                            <th class="py-2 px-4 text-left border">View</th>
+                            <th class="py-2 px-4 text-left border">Edit</th>
+                            <th class="py-2 px-4 text-left border">Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-if="menus.data.length === 0">
+                            <td colspan="6" class="py-2 px-4 text-center">No products found.</td>
+                        </tr>
+                        <tr
+                            v-for="(item, index) in menus.data"
+                            :key="index"
+                        >
+                            <td class="py-2 px-4 border">{{ item.id }}</td>
+                            <td class="py-2 px-4 border">{{ item.name }}</td>
+                            <td class="py-2 px-4 border">{{ item.price }}</td>
+                            <td class="py-2 px-4 border">
+                                <Link 
+                                    :href="route('admin.menu.show', item.id)" 
+                                    class="px-2 py-1 text-sm bg-blue-300 text-white mr-2 rounded inline-block">
+                                    Show
+                                </Link>
+                            </td>
+                            <td class="py-2 px-4 border">
+                                <Link 
+                                    :href="route('admin.menu.edit', item.id)" 
+                                    class="px-2 py-1 text-sm bg-green-500 text-white mr-2 rounded inline-block">
+                                    Edit
+                                </Link>
+                            </td>
+                            <td class="py-2 px-4 border">
+                                <button
+                                    type="submit"
+                                    class="px-2 py-1 text-sm bg-red-500 text-white mr-2 rounded inline-block"
+                                    @click="deleteMenu(item.id)"    
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- Pagination -->
+            <div class="flex justify-center mt-4">
+                <Link
+                    v-for="(link, index) in menus.links"
+                    :key="index"
+                    :href="link.url || '#'"
+                    v-html="link.label"
+                    :class="{
+                        'text-gray-500 cursor-not-allowed': !link.url, 
+                        'font-bold': link.active,
+                        'hover:bg-gray-200': link.url
+                    }"
+                    class="px-2 py-1 mx-1 rounded border border-gray-300"
+                />
+            </div>
         </div>
     </AdminAuthenticatedLayout>
 </template>
@@ -68,7 +118,8 @@ import AdminAuthenticatedLayout from '@/Layouts/AdminAuthenticatedLayout.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 
 defineProps({
-    menus: Array,
+    menus: Object,
+    search: String,
 });
 
 const form = useForm({});
